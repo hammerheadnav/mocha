130c9a84542c7055cb7a2c345ba90e04ebaee7fc
Bharath MG <mgbharath@ymail.com>
2017-03-09 16:32:21 +0530

    Added setText and clearText actions in DSL

--- mocha-api/src/main/kotlin/io/hammerhead/mocha/api/uiactions/espresso/EspressoActionsDelegator.kt
+++ mocha-api/src/main/kotlin/io/hammerhead/mocha/api/uiactions/espresso/EspressoActionsDelegator.kt
@@ -2,7 +2,7 @@ package io.hammerhead.mocha.api.uiactions.espresso
 
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.ViewAction
-import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.*
 import io.hammerhead.mocha.dsl.DslTagMarker
 import org.hamcrest.core.AllOf.allOf
 
@@ -10,6 +10,10 @@ import org.hamcrest.core.AllOf.allOf
 class EspressoActionsDelegator {
 
     internal fun clickDelegate(init: EspressoUiAction.() -> Unit) = delegate(init, { click() })
+
+    internal fun setTextDelegate(stringToSet: String, init: EspressoUiAction.() -> Unit) = delegate(init, { typeText(stringToSet) })
+
+    internal fun clearTextDelegate(init: EspressoUiAction.() -> Unit) = delegate(init, { clearText() })
 
     private fun delegate(init: EspressoUiAction.() -> Unit, action: (() -> ViewAction)) {
         EspressoUiAction(init, {
--- mocha-api/src/main/kotlin/io/hammerhead/mocha/dsl/EspressoActionsDsl.kt
+++ mocha-api/src/main/kotlin/io/hammerhead/mocha/dsl/EspressoActionsDsl.kt
@@ -8,6 +8,14 @@ fun EspressoActionsDelegator.click(init: EspressoUiAction.() -> Unit) {
     this.clickDelegate(init)
 }
 
+fun EspressoActionsDelegator.setText(stringToSet: String, init: EspressoUiAction.() -> Unit) {
+    this.setTextDelegate(stringToSet, init)
+}
+
+fun EspressoActionsDelegator.clearText(init: EspressoUiAction.() -> Unit){
+    this.clearTextDelegate(init)
+}
+
 fun EspressoActionsDelegator.expect(init: EspressoExpectDelegator.() -> Unit) {
     EspressoExpectDelegator().init()
 }
