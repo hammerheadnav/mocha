a92b276e4df82e8e0843eb140ecd33754cb4ec50
Bharath MG <mgbharath@ymail.com>
2017-03-10 15:42:26 +0530

    Added a image drawable espresso selector

--- mocha-api/src/main/kotlin/io/hammerhead/mocha/api/uiactions/espresso/EspressoUiAction.kt
+++ mocha-api/src/main/kotlin/io/hammerhead/mocha/api/uiactions/espresso/EspressoUiAction.kt
@@ -3,7 +3,7 @@ package io.hammerhead.mocha.api.uiactions.espresso
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.matcher.ViewMatchers
 import android.view.View
-import io.hammerhead.mocha.api.expectations.espresso.withBackground
+import io.hammerhead.mocha.api.expectations.espresso.withImageDrawable
 import io.hammerhead.mocha.api.uiactions.SideEffectDelegator
 import io.hammerhead.mocha.dsl.DslTagMarker
 import org.hamcrest.Matcher
@@ -18,7 +18,7 @@ class EspressoUiAction(init: EspressoUiAction.() -> Unit, val action: (List<Matc
     var text: String by SideEffectDelegator { matchersList.add(ViewMatchers.withText(it)) }
     var assignableFrom: Class<out View> by SideEffectDelegator { matchersList.add(ViewMatchers.isAssignableFrom(it)) }
     var textStartsWith: String by SideEffectDelegator { matchersList.add(ViewMatchers.withText(startsWith(it))) }
-    var backgroundDrawableId: Int by SideEffectDelegator { matchersList.add(withBackground(it)) }
+    var imageDrawableId: Int by SideEffectDelegator { matchersList.add(withImageDrawable(it)) }
 
     init {
         init()
