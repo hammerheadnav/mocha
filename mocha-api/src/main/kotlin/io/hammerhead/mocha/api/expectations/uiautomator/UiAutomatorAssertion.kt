1170c349302e13f83120286d62316e1a191c2b03
Arun <arun111090@gmail.com>
2017-06-15 18:42:08 +0530

    [Arun] Refactored exists, waitForExists methods in UiAutomatorAssertion

--- mocha-api/src/main/kotlin/io/hammerhead/mocha/api/expectations/uiautomator/UiAutomatorAssertion.kt
+++ mocha-api/src/main/kotlin/io/hammerhead/mocha/api/expectations/uiautomator/UiAutomatorAssertion.kt
@@ -6,8 +6,8 @@ import io.hammerhead.mocha.api.uiactions.SideEffectDelegator
 
 class UiAutomatorAssertion(init: UiAutomatorAssertion.() -> Unit, private val uiObject: UiObject) {
     var text: String by SideEffectDelegator { check(uiObject.text == it, { "Expected $it Actual ${uiObject.text}" }) }
-    var exists: Boolean by SideEffectDelegator { check(uiObject.exists() == exists) }
-    var waitForExists: Long by SideEffectDelegator { check(uiObject.waitForExists(it) == exists)  }
+    var exists: Boolean by SideEffectDelegator { check(uiObject.exists() == it) }
+    var waitForExists: Long by SideEffectDelegator { check(uiObject.waitForExists(it) == true)  }
 
     init {
         init()
